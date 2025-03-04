45aafb223a1124164cb9ca2d459321ce1024d1ce
xu3maru <103827479+welzd@users.noreply.github.com>
2025-03-04 22:41:03 +0000

    Fix setkube.sh

--- setkube.sh
+++ setkube.sh
@@ -3,7 +3,6 @@
 # In the future I will make that script more customizable
 
 # Color codes section
-RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
@@ -26,7 +25,10 @@ echo -e "${GREEN}[+] Packages upgrade ${NC}"
 sudo apt-get upgrade
 
 # Changing the hostname to fit the needs 
-echo -e "${YELLOW}[+] Hostname changing ${NC}"
+echo -e "${GREEN}[+] Hostname changing ${NC}"
+read -p "Modify the hostname(default=master) : " new_hostname
+new_hostname=${new_hostname:-master}
+sudo hostnamectl set-hostname $new_hostname
 
 # First let's backup the /etc/hosts file
 echo -e "${GREEN}[+] Creating a backup file for the hosts file ${NC}"
@@ -37,12 +39,12 @@ echo -e "${YELLOW}[+] Creating a backup file for the hosts file ${NC}"
 sudo mv $NEW_HOSTS $ETC_HOSTS
 
 # Installing the container runtime and enable it at startup
-echo -e "${YELLOW}[+] Container runtime setup (containerd) ${NC}"
+echo -e "${GREEN}[+] Container runtime setup (containerd) ${NC}"
 sudo apt-get install containerd
 sudo systemctl enable containerd
 
 # Charging modules for containerd
-echo -e "${YELLOW}[+] Setting up the modules for containerd ${NC}"
+echo -e "${GREEN}[+] Setting up the modules for containerd ${NC}"
 echo "overlay \nbr_netfilter" | sudo tee /etc/modules-load.d/containerd.conf > /dev/null
 sudo modprobe overlay
 sudo modprobe br_netfilter
@@ -52,14 +54,14 @@ echo -e "${YELLOW}[+] Starting the CRI ${NC}"
 sudo systemctl start containerd
 
 # Setting systemd cgroup drivers
-echo -e "${YELLOW}[+] Systemd Cgroup drivers setup ${NC}"
+echo -e "${GREEN}[+] Systemd Cgroup drivers setup ${NC}"
 sudo mkdir /etc/containerd
 sudo containerd config default > config.toml
 sudo mv config.toml /etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
 
 # Install the CNI plugin for containerd nertwork purpose
-echo -e "${YELLOW}[+] Running containerd setup ${NC}"
+echo -e "${GREEN}[+] Running containerd setup ${NC}"
 sudo apt-get install curl
 wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
 mkdir -p /opt/cni/bin  
@@ -75,7 +77,7 @@ echo -e "${YELLOW}[+] Applying the changes to the system  ${NC}"
 sudo sysctl --system
 
 # Installing the kube tools - kubelet kubeadm kubectl
-echo -e "${YELLOW}[+] Dependencies setup  ${NC}"
+echo -e "${GREEN}[+] Dependencies setup  ${NC}"
 sudo apt-get install apt-transport-https ca-certificates curl gpg
 
 echo -e "${YELLOW}[+] Adding keyrings ${NC}"
@@ -85,9 +87,8 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.31/deb/Release.key | sudo gpg --
 echo -e "${YELLOW}[+] Adding the Kubernetes apt repository ${NC}"
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.31/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-echo -e "${YELLOW}[+] Installing Kubernetes tools and enabling kubelet ${NC}"
+echo -e "${GREEN}[+] Installing Kubernetes tools and enabling kubelet ${NC}"
 sudo apt-get update
 sudo apt-get install kubeadm kubelet kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
-
