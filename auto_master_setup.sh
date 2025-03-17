4d34d413f5aa1327c9d82d4cf465fada2a0d312d
xu3maru <103827479+welzd@users.noreply.github.com>
2025-03-17 21:13:50 +0000

    making changes to the cluster init command

--- auto_master_setup.sh
+++ auto_master_setup.sh
@@ -5,7 +5,7 @@ BRIGHT_RED='\033[1;31m'
 NC='\033[0m' 
 
 echo -e "${MAGENTA}[🤖] Init the master node of the cluster ${NC}"
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16 -v 4
+sudo kubeadm init -v=5 --upload-certs --control-plane-endpoint master:6443 --pod-network-cidr=192.168.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
